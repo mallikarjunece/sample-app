4af397d21db7e59b6267a2836d86bc71fc616fc6
Mallikarjuna <mallikarjunece@gmail.com>
2019-03-29 10:28:36 +0530

    scaffold test

---
+++ db/migrate/20190329045613_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.strinh :author
+      t.integer :price
+      t.integer :version
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
