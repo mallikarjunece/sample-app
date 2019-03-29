465022a6f5835f4c09299747640770bafe5d73bd
Mallikarjuna <mallikarjunece@gmail.com>
2019-03-29 10:41:43 +0530

    Update 20190329045613_create_books.rb
    syntax fix

--- db/migrate/20190329045613_create_books.rb
+++ db/migrate/20190329045613_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title
-      t.strinh :author
+      t.string :author
       t.integer :price
       t.integer :version
       t.string :category
